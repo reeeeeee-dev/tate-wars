3b75ca77a8646e2293c825d523434c8c11ed7799
Reetik Patel <reetikp@gmail.com>
2024-06-12 22:50:43 -0500

    🎉 init

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { useState } from 'react';
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,34 +9,14 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const [page, setPage] = useState(1)
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <h1 className='text-3xl font-bold underline'>
+        Hello World!
+      </h1>
+      
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const isStorybook = process.argv[1]?.includes('storybook')
+
 export default defineConfig({
   plugins: [
-    remix({
+    !isStorybook && remix({
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
